4951243ad17ae96e2b9f51e5810641557e187029
Medlabs <mlbsaad@gmail.com>
2024-11-13 23:58:28 +0100

    fixed shortcuts icons

--- src/components/Files.tsx
+++ src/components/Files.tsx
@@ -1,17 +1,28 @@
-import { onMount, onCleanup, createSignal, Show, For, type Component } from 'solid-js';
-import { Icon } from '@iconify-icon/solid';
-import dnd from '../utils/dnd';
-
+import {
+  type Component,
+  createSignal,
+  For,
+  onCleanup,
+  onMount,
+  Show,
+} from "solid-js";
+import { Icon } from "@iconify-icon/solid";
+import dnd from "../utils/dnd";
 
 const Files: Component<{ toggle: (param: string) => void }> = (props) => {
-
   let files: HTMLDivElement;
-  let [filesPos, setFilesPos] = createSignal({ top: window.innerHeight, left: window.innerWidth / 3 });
+  let [filesPos, setFilesPos] = createSignal({
+    top: window.innerHeight,
+    left: window.innerWidth / 3,
+  });
   let [windowMaximized, setWindowMaximized] = createSignal(false);
 
   onMount(() => {
     if (files) {
-      setFilesPos({ top: Math.random() * (window.innerHeight - files.offsetHeight), left: Math.random() * (window.innerWidth - files.offsetWidth) });
+      setFilesPos({
+        top: Math.random() * (window.innerHeight - files.offsetHeight),
+        left: Math.random() * (window.innerWidth - files.offsetWidth),
+      });
       files.style.top = `${filesPos().top}px`;
       files.style.left = `${filesPos().left}px`;
     }
@@ -21,57 +32,60 @@ const Files: Component<{ toggle: (param: string) => void }> = (props) => {
 
   const shortcuts = [
     {
-      title: 'Home',
-      icon: 'carbon:home'
+      title: "Home",
+      icon: "carbon:home",
     },
     {
-      title: 'Documents',
-      icon: 'home'
+      title: "Documents",
+      icon: "carbon:document",
     },
     {
-      title: 'Pictures',
-      icon: 'home'
+      title: "Pictures",
+      icon: "carbon:image",
     },
     {
-      title: 'Downloads',
-      icon: 'home'
+      title: "Downloads",
+      icon: "carbon:download",
     },
     {
-      title: 'Videos',
-      icon: 'home'
+      title: "Videos",
+      icon: "carbon:video",
     },
     {
-      title: 'Projects',
-      icon: 'home'
-    }
+      title: "Projects",
+      icon: "carbon:application-web",
+    },
   ];
 
   function maximize(element: string) {
     setWindowMaximized(true);
     let div = document.getElementById(element);
-    div?.classList.add('w-screen');
-    div?.classList.add('h-screen');
-    div?.classList.remove('w-[50vw]');
-    div?.classList.remove('h-[40vh]');
-    div!.style.left = '0px';
-    div!.style.top = '0px';
+    div?.classList.add("w-screen");
+    div?.classList.add("h-screen");
+    div?.classList.remove("w-[50vw]");
+    div?.classList.remove("h-[40vh]");
+    div!.style.left = "0px";
+    div!.style.top = "0px";
   }
   function restore(element: string) {
     setWindowMaximized(false);
     let div = document.getElementById(element);
-    div?.classList.remove('w-screen');
-    div?.classList.remove('h-screen');
-    div?.classList.add('w-[45vw]');
-    div?.classList.add('h-[40vh]');
+    div?.classList.remove("w-screen");
+    div?.classList.remove("h-screen");
+    div?.classList.add("w-[45vw]");
+    div?.classList.add("h-[40vh]");
     div!.style.top = `${filesPos().top}px`;
     div!.style.left = `${filesPos().left}px`;
   }
   function closeAndMinimize() {
-    props.toggle('files');
+    props.toggle("files");
   }
 
   return (
-    <div ref={files!} class={`w-[45vw] h-[40vh] flex justify-start absolute shadow `}>
+    <div
+      ref={files!}
+      class={`w-[45vw] h-[40vh] flex justify-start absolute shadow `}
+    >
       <div class="h-full w-1/4 bg-neutral-100/75 backdrop-blur-md pt-2 rounded-tl-lg rounded-bl-lg">
         <div class="flex justify-start h-8 p-2 items-center text-white active:cursor-move">
           <button
@@ -85,7 +99,9 @@ const Files: Component<{ toggle: (param: string) => void }> = (props) => {
           </button>
           <button
             class="flex justify-center items-center w-4 h-4 bg-green-400 rounded-full mr-1"
-            onclick={() => (windowMaximized() ? restore('files') : maximize('files'))}
+            onclick={() => (windowMaximized()
+              ? restore("files")
+              : maximize("files"))}
           >
             <Show when={windowMaximized()}>
               <Icon icon="carbon:minimize" class="text-xs" />
@@ -97,23 +113,28 @@ const Files: Component<{ toggle: (param: string) => void }> = (props) => {
         </div>
         <For each={shortcuts}>
           {(s: any) => (
-            <div class="text-sm w-full text-neutral-700 p-1 hover:bg-neutral-100/25 hover:cursor-default">
-              <Icon icon={s.icon} /><span>{s.title} </span>
+            <div class="flex justify-start items-center text-sm w-full text-neutral-800 p-1 hover:bg-neutral-100/25 hover:cursor-default">
+              <Icon icon={s.icon} class="mr-2" />
+              <span>{s.title}</span>
             </div>
           )}
         </For>
       </div>
       <div class="bg-neutral-100 w-full rounded-tr-lg rounded-br-lg">
-        <div
-          class="flex h-10 p-2 items-center rounded-tr-lg text-white border-b border-neutral-300 bg-neutral-200 active:cursor-move"
-        >
-          <Icon icon="carbon:chevron-left" class="text-2xl text-neutral-700" />
-          <Icon icon="carbon:chevron-right" class="ml-2 text-2xl text-neutral-700" />
+        <div class="flex h-10 p-2 items-center rounded-tr-lg text-white border-b border-neutral-300 bg-neutral-200 active:cursor-move">
+          <Icon
+            icon="carbon:chevron-left"
+            class="text-2xl text-neutral-700"
+          />
+          <Icon
+            icon="carbon:chevron-right"
+            class="ml-2 text-2xl text-neutral-700"
+          />
           <div class="ml-4 text-neutral-700 text-center font-bold">Files</div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Files
+export default Files;
